e485d29b1dc49f11031682817417d61eeba0b371
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-07-31 03:31:58 +0530

    added auth components

---
+++ app/auth/signin/page.tsx
@@ -0,0 +1,9 @@
+import WelcomeComponent from "@/components/WelcomeComponent";
+
+export default function signIn() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <WelcomeComponent />
+    </div>
+  );
+}
