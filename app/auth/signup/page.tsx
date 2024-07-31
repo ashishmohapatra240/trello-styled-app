3b474411d3b68509f958932612f5981ab640f79b
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2024-07-31 20:14:20 +0530

    moved to frontend

--- app/auth/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import WelcomeComponent from "@/components/AuthComponents/WelcomeComponent";
-
-export default function signIn() {
-  return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <WelcomeComponent />
-    </div>
-  );
-}
